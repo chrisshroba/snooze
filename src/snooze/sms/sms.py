b2f48fd47183c9f9e4182a828cef5609762c158e
Chris Shroba <chrisshroba@gmail.com>
2015-06-04 08:54:27 -0500

    Started using Plivo example code

--- src/snooze/sms/sms.py
+++ src/snooze/sms/sms.py
@@ -1,1 +1,20 @@
 __author__ = 'chrisshroba'
+
+
+import plivo
+
+
+auth_id = ""
+auth_token = ""
+
+p = plivo.RestAPI(auth_id, auth_token)
+
+# Send a SMS
+params = {
+    'src': '1202XXXXXX', # Caller Id
+    'dst' : '12033XXXXX', # User Number to Call
+    'text' : "Hi, message from Plivo",
+    'type' : "sms",
+}
+
+response = p.send_message(params)
